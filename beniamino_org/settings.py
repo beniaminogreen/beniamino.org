54dad67f518d46069531e9ac838fae7a57e08810
Ben Green <ben@greendalba.com>
2019-05-03 17:21:21 +0100

    added enviroment variables to settings

--- beniamino_org/settings.py
+++ beniamino_org/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lw_w77^+@ek8rlzxce4xyp%8^13cxl+f*v+4%j(z72ae^y$=(w'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #os.environ.get("DJANGO_DEBUG") == "True"
+DEBUG = os.environ.get("DJANGO_DEBUG") == "True"
 
 ALLOWED_HOSTS = ['*']
 
