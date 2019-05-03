da3fb895076addcd01e1b4bed73546647730f822
Ben Green <ben@greendalba.com>
2019-05-03 18:07:39 +0100

    changed static files

--- beniamino_org/settings.py
+++ beniamino_org/settings.py
@@ -140,6 +140,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 django_heroku.settings(locals())
 
 
