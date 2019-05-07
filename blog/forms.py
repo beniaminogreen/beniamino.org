210468737f61c6e680b85124a1e82cd3cecf1d55
beniamino green <ben@greendalba.com>
2019-05-07 22:58:07 +0100

    resized contact page

--- blog/forms.py
+++ blog/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ContactForm(forms.Form):
     from_email = forms.EmailField(required=False)
     name = forms.CharField(max_length=200)
-    body = forms.CharField(widget=forms.Textarea(attrs={'cols': 90, 'rows': 10}), required=True)
+    body = forms.CharField(widget=forms.Textarea(attrs={'size': 20}), required=True)
     
